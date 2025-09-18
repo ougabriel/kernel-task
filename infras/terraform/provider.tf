aeeb43645aaa80c931f8fd4bab7aa2272d95d6f6
GABRIEL OKOM <34310658+ougabriel@users.noreply.github.com>
2025-09-18 01:54:45 +0100

    Updated provider.tf

--- infras/terraform/provider.tf
+++ infras/terraform/provider.tf
@@ -1,15 +1,17 @@
-provider "aws" {
-  region = var.aws_region
-  profile = var.aws_profile
-
-}
-
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+  
+  default_tags {
+    tags = var.common_tags
+  }
 }
